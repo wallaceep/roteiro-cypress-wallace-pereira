5afa59c1afa01672d19aa0f02503ff68dda07789
Wallace <wallacepereira2812@gmail.com>
2024-07-03 21:37:37 -0300

    Testes adicionados

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -56,4 +56,57 @@ describe('template spec', () => {
     cy.get('.todo-list li')
       .should('have.length', 2);
   });
+
+  it('Edita uma tarefa com clique duplo', () => {
+    cy.visit('http://127.0.0.1:7001'); 
+
+    cy.get('.new-todo')
+      .type('Tarefa para editar{enter}');
+
+    cy.get('.todo-list li')
+      .first()
+      .dblclick();
+
+    cy.get('.todo-list li')
+      .first()
+      .find('input.edit')
+      .should('have.value', 'Tarefa para editar')
+      .clear()
+      .type('Tarefa editada{enter}');
+
+    cy.get('.todo-list li')
+      .first()
+      .should('contain', 'Tarefa editada');
+  });
+
+  it('Limpa todas as tarefas completadas', () => {
+    cy.visit('http://127.0.0.1:7001');
+    cy.get('.new-todo')
+      .type('Tarefa 1{enter}')
+      .type('Tarefa 2{enter}')
+      .type('Tarefa 3{enter}');
+    cy.get('.todo-list li .toggle').eq(0).click();
+    cy.get('.todo-list li .toggle').eq(2).click();
+    cy.get('.todo-list li').should('have.length', 3);
+    cy.contains('Clear completed').click();
+    cy.get('.todo-list li').should('have.length', 1);
+    cy.get('.todo-list li').should('have.text', 'Tarefa 2');
+  });
+
+  it('Marca todas as tarefas como concluídas clicando na seta para baixo', () => {
+    cy.visit('http://127.0.0.1:7001'); 
+
+    cy.get('.new-todo')
+      .type('Tarefa 1{enter}')
+      .type('Tarefa 2{enter}')
+      .type('Tarefa 3{enter}');
+
+    cy.get('.toggle-all-label').click(); // Altere para o seletor correto da seta para baixo
+
+    cy.get('.todo-list li')
+      .should('have.length', 3)
+      .each(($el) => {
+        cy.wrap($el).should('have.class', 'completed');
+      });
+  });
 });
